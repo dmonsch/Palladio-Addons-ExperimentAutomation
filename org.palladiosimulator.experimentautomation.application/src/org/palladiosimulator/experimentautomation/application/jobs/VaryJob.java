fe77860a98f89ecb7db61bb4ec56407e0841f60e
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-07-23 18:46:37 +0000

    Now actually varying the model in the blackboard :)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/ExperimentAutomation/trunk@23643 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/jobs/VaryJob.java
+++ org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/jobs/VaryJob.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.palladiosimulator.experimentautomation.application.utils.EcoreHelper;
 import org.palladiosimulator.experimentautomation.application.variation.IVariationStrategy;
 import org.palladiosimulator.experimentautomation.application.variation.VariationStrategyFactory;
@@ -14,6 +13,8 @@ import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
 import de.uka.ipd.sdq.workflow.jobs.SequentialBlackboardInteractingJob;
 import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
+import de.uka.ipd.sdq.workflow.pcm.jobs.LoadPCMModelsIntoBlackboardJob;
 
 /**
  * This job conducts a given experiment for a given analysis tool. The job assumes that involved
@@ -25,7 +26,7 @@ public class VaryJob extends SequentialBlackboardInteractingJob<MDSDBlackboard> 
 
     private final List<Variation> variations;
     private final List<Long> factorLevels;
-    
+
     /**
      * Default Constructor.
      * 
@@ -34,8 +35,7 @@ public class VaryJob extends SequentialBlackboardInteractingJob<MDSDBlackboard> 
      * @param factorLevels
      *            Factor levels considered for the experiment.
      */
-    public VaryJob(final List<Variation> variations,
-            final List<Long> factorLevels) {
+    public VaryJob(final List<Variation> variations, final List<Long> factorLevels) {
         super(false);
 
         this.variations = variations;
@@ -44,29 +44,28 @@ public class VaryJob extends SequentialBlackboardInteractingJob<MDSDBlackboard> 
 
     @Override
     public void execute(final IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
-        // FIXME Crucial fixme!
-        // copy initial PCM model
-        //ExperimentAutomationConfiguration clonedConfiguration = configuration.clone();
-
-        // modify the copied PCM model according to the variation descriptions
+        // modify the copied PCM model according to the variation descriptions; one by one
         for (int i = 0; i < this.variations.size(); i++) {
             final Variation variation = this.variations.get(i);
             final Long currentValue = this.factorLevels.get(i);
-//            final IVariationStrategy variationStrategy = this.initialiseVariations(variation,
-//                    clonedConfiguration.getResourceSet()); // FIXME Modify in blackboard?
-//
-//            variationStrategy.vary(currentValue);
+
+            this.executeVariation(variation, currentValue);
         }
     }
 
-    private IVariationStrategy initialiseVariations(final Variation variation, final ResourceSet resourceSet) {
-        final EObject variedObject = EcoreHelper.findModelElementById(resourceSet, variation.getVariedObjectId());
+    private void executeVariation(final Variation variation, Long currentValue) {
+        ResourceSetPartition pcmPartition = this.getBlackboard().getPartition(
+                LoadPCMModelsIntoBlackboardJob.PCM_MODELS_PARTITION_ID);
+        final EObject variedObject = EcoreHelper.findModelElementById(pcmPartition.getResourceSet(),
+                variation.getVariedObjectId());
+        
         if (variedObject == null) {
             throw new RuntimeException("Unable to find model element with ID " + variation.getVariedObjectId()
                     + " in resource set");
         }
+
         final IVariationStrategy variationStrategy = VariationStrategyFactory.createStrategy(variation.getType());
         variationStrategy.setVariedObject(variedObject);
-        return variationStrategy;
+        variationStrategy.vary(currentValue);
     }
 }
