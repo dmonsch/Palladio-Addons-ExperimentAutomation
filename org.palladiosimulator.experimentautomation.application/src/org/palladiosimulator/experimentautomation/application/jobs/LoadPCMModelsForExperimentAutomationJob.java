53d94f3673a77e28ab3ba54691f12fce404242ce
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-07-24 12:54:25 +0000

    Some initial code for PMS and SD loading
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/ExperimentAutomation/trunk@23668 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/jobs/LoadPCMModelsForExperimentAutomationJob.java
+++ org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/jobs/LoadPCMModelsForExperimentAutomationJob.java
@@ -1,18 +1,29 @@
 package org.palladiosimulator.experimentautomation.application.jobs;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.palladiosimulator.experimentautomation.experiments.InitialModel;
+import org.palladiosimulator.experimentautomation.experiments.ReconfigurationRulesFolder;
+import org.palladiosimulator.simulizar.launcher.jobs.LoadPMSModelIntoBlackboardJob;
+import org.palladiosimulator.simulizar.launcher.jobs.LoadSDMModelsIntoBlackboardJob;
+import org.palladiosimulator.simulizar.launcher.partitions.PMSResourceSetPartition;
+import org.palladiosimulator.simulizar.launcher.partitions.SDMResourceSetPartition;
+import org.palladiosimulator.simulizar.pms.PMSModel;
 
-import de.uka.ipd.sdq.workflow.jobs.CleanupFailedException;
-import de.uka.ipd.sdq.workflow.jobs.IBlackboardInteractingJob;
-import de.uka.ipd.sdq.workflow.jobs.IJob;
 import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
 import de.uka.ipd.sdq.workflow.jobs.SequentialBlackboardInteractingJob;
 import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
+import de.uka.ipd.sdq.workflow.pcm.blackboard.PCMResourceSetPartition;
 import de.uka.ipd.sdq.workflow.pcm.jobs.LoadPCMModelsIntoBlackboardJob;
 
 /**
@@ -23,12 +34,11 @@ import de.uka.ipd.sdq.workflow.pcm.jobs.LoadPCMModelsIntoBlackboardJob;
  * 
  * @author Sebastian Lehrig
  */
-public class LoadPCMModelsForExperimentAutomationJob extends SequentialBlackboardInteractingJob<MDSDBlackboard>
-        implements IJob, IBlackboardInteractingJob<MDSDBlackboard> {
-    private static final Logger LOGGER = Logger.getLogger(LoadPCMModelsForExperimentAutomationJob.class);
-    private final InitialModel initialModel;
+public class LoadPCMModelsForExperimentAutomationJob extends SequentialBlackboardInteractingJob<MDSDBlackboard> {
 
-    private MDSDBlackboard blackboard;
+    private static final Logger LOGGER = Logger.getLogger(LoadPCMModelsForExperimentAutomationJob.class);
+
+    private final InitialModel initialModel;
 
     public LoadPCMModelsForExperimentAutomationJob(InitialModel initialModel) {
         super(false);
@@ -37,14 +47,9 @@ public class LoadPCMModelsForExperimentAutomationJob extends SequentialBlackboar
 
     @Override
     public void execute(final IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
-        ResourceSetPartition pcmPartition = this.blackboard
-                .getPartition(LoadPCMModelsIntoBlackboardJob.PCM_MODELS_PARTITION_ID);
-        ResourceSetPartition middlewarePartition = this.blackboard
-                .getPartition(LoadPCMModelsIntoBlackboardJob.MIDDLEWARE_PARTITION_ID);
-        ResourceSetPartition eventMiddlewarePartition = this.blackboard
-                .getPartition(LoadPCMModelsIntoBlackboardJob.EVENT_MIDDLEWARE_PARTITION_ID);
-
         // Load the PCM model itself
+        final ResourceSetPartition pcmPartition = this.getBlackboard().getPartition(
+                LoadPCMModelsIntoBlackboardJob.PCM_MODELS_PARTITION_ID);
         if (LOGGER.isEnabledFor(Level.INFO)) {
             LOGGER.info("Loading PCM models");
         }
@@ -53,6 +58,8 @@ public class LoadPCMModelsForExperimentAutomationJob extends SequentialBlackboar
         pcmPartition.resolveAllProxies();
 
         // load the middleware completion
+        final ResourceSetPartition middlewarePartition = this.getBlackboard().getPartition(
+                LoadPCMModelsIntoBlackboardJob.MIDDLEWARE_PARTITION_ID);
         if (LOGGER.isEnabledFor(Level.INFO)) {
             LOGGER.info("Loading middleware completion models");
         }
@@ -60,11 +67,68 @@ public class LoadPCMModelsForExperimentAutomationJob extends SequentialBlackboar
         middlewarePartition.resolveAllProxies();
 
         // load the event middleware repository
+        final ResourceSetPartition eventMiddlewarePartition = this.getBlackboard().getPartition(
+                LoadPCMModelsIntoBlackboardJob.EVENT_MIDDLEWARE_PARTITION_ID);
         if (LOGGER.isEnabledFor(Level.INFO)) {
             LOGGER.info("Loading event middleware models");
         }
         eventMiddlewarePartition.loadModel(this.initialModel.getEventMiddleWareRepository().eResource().getURI());
         eventMiddlewarePartition.resolveAllProxies();
+
+        // configure partition & load PMS model
+        final PMSResourceSetPartition pmsPartition = new PMSResourceSetPartition((PCMResourceSetPartition) pcmPartition);
+        final PMSModel pmsModel = this.initialModel.getPlatformMonitoringSpecification();
+        if (pmsModel != null) {
+            pmsPartition.loadModel(pmsModel.eResource().getURI());
+        }
+        this.getBlackboard().addPartition(LoadPMSModelIntoBlackboardJob.PMS_MODEL_PARTITION_ID, pmsPartition);
+        pmsPartition.resolveAllProxiesToPCM();
+
+        // configure partition & load SDM models
+        final SDMResourceSetPartition sdmPartition = new SDMResourceSetPartition();
+        final ReconfigurationRulesFolder reconfigurationRulesFolder = this.initialModel.getReconfigurationRules();
+
+        this.getBlackboard().addPartition(LoadSDMModelsIntoBlackboardJob.SDM_MODEL_PARTITION_ID, sdmPartition);
+        if (reconfigurationRulesFolder != null) {
+
+            // add file protocol only if necessary
+            File folder = null;
+            if (!reconfigurationRulesFolder.getFolderUri().startsWith("platform:")) {
+                final String filePath = "file:///" + reconfigurationRulesFolder.getFolderUri();
+                final URI pathToSDM = URI.createURI(filePath);
+
+                folder = new File(pathToSDM.toFileString());
+            } else {
+                try {
+                    final URL pathURL = FileLocator.resolve(new URL(reconfigurationRulesFolder.getFolderUri()));
+                    final String folderString = pathURL.toExternalForm().replace("file:", "");
+
+                    folder = new File(folderString);
+                } catch (IOException e) {
+                    LOGGER.warn("No SDM models found, SD reconfigurations disabled.", e);
+                    return;
+                }
+            }
+
+            if (!folder.exists()) {
+                LOGGER.warn("Folder " + folder + " does not exist. No reconfiguration rules will be loaded.");
+                return;
+            }
+            final File[] files = folder.listFiles(new FilenameFilter() {
+
+                @Override
+                public boolean accept(final File dir, final String name) {
+                    return name.endsWith(LoadSDMModelsIntoBlackboardJob.STORYDIAGRAMS_FILE_EXTENSION);
+                }
+            });
+            if (files != null && files.length > 0) {
+                for (final File file : files) {
+                    sdmPartition.loadModel(URI.createFileURI(file.getPath()));
+                }
+            } else {
+                LOGGER.warn("No SDM models found, SD reconfigurations disabled.");
+            }
+        }
     }
 
     /**
@@ -73,20 +137,5 @@ public class LoadPCMModelsForExperimentAutomationJob extends SequentialBlackboar
     @Override
     public String getName() {
         return "Perform PCM Model Load";
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setBlackboard(MDSDBlackboard blackboard) {
-        this.blackboard = blackboard;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void cleanup(final IProgressMonitor monitor) throws CleanupFailedException {
     }
 }
