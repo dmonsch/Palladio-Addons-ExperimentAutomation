8c3ca6608a256a5f7121874e7a64bdf025c854d3
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-09-09 07:05:34 +0000

    Using extension Helper class
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/ExperimentAutomation/trunk@24488 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/tooladapter/AnalysisToolFactory.java
+++ org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/tooladapter/AnalysisToolFactory.java
@@ -5,8 +5,23 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 import org.palladiosimulator.experimentautomation.experiments.ToolConfiguration;
 
+/**
+ * Creates an analysis tool adapter object by getting it from an extension point.
+ * 
+ * TODO ExtensionHelper should optimally be used. However, ExtensionHelper seems to miss some
+ * advanced filtering options as used here. Java 1.8 Lambda expressions could help. [Lehrig]
+ * 
+ * @author Sebastian Lehrig
+ */
 public class AnalysisToolFactory {
 
+    /**
+     * Creates a tool adapter by creating an executable extension based from an extension.
+     * 
+     * @param configuration
+     *            the tool configuration stating which tool shall be created.
+     * @return the created tool adapter.
+     */
     public static IToolAdapter createToolAdapater(final ToolConfiguration configuration) {
         assert configuration != null : "The configuration may not be null";
 
